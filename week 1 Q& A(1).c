d67eeb04bd5a035c914e579245b87df12a5b2368
Madhusmita Sahoo <99548052+Shikha2002@users.noreply.github.com>
2022-02-12 21:03:54 +0530

    Add files via upload

---
+++ week 1 Q& A(1).c
@@ -0,0 +1,9 @@
+//c program to display your basic information 
+#include<stdio.h>
+int  main()
+{
+printf("My name is Madhusmita Sahoo\n");
+printf("My registration no is 065\n");
+printf("Branch:Civil Engineering \n");
+printf("My hobby is novel reading\n");
+return 0;}
